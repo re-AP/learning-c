c6e7109d7a8a7cc718de8e159ea27a38ae3e0600
re-AP <ap.official1105@gmail.com>
2026-06-22 12:44:07 +0530

    Lesson 1.3 with exe

--- practice/scratch.c
+++ practice/scratch.c
@@ -2,19 +2,11 @@
 
 /* A table to print centigrade temp value after converting from fahrenheit temp values */
 int main (void){
-	int lower , upper , step ;
-	float fahr , cels ;
+	int fahr;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
+	for(fahr = 0; fahr <= 300 ; fahr = fahr + 20){
+		printf("%3.0d\t%6.1f\n" , fahr , (5.0/9.0)*(fahr - 32));
 
-	fahr = lower;
-
-	while (fahr <= 300){
-		cels = (5.0/9.0)*(fahr - 32.0);
-		printf("%f\t%f\n", fahr, cels);
-		fahr = fahr + step;
 	}
 
 }
